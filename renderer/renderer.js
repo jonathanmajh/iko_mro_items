c13c990ebd383a746aae44fc43177a845b7a158b
Jonathan Ma <jonathanmajh@gmail.com>
2020-12-23 12:39:02 -0500

    result table, toasts, custom copy

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -5,16 +5,19 @@
 // selectively enable features needed in the rendering
 // process.
 
-const { ipcRenderer } = require('electron')
+const { ipcRenderer, clipboard } = require('electron')
 const worker = new Worker('./worker.js')
+// TODO each process should spawn and kill their own worker
 
 document.getElementById("valid-single").addEventListener("click", validSingle);
 document.getElementById("valid-triple").addEventListener("click", progress);
 document.getElementById("batch-file").addEventListener("click", test);
 document.getElementById("single-input").addEventListener("hide.bs.collapse", stub);
-document.getElementById("batch-input").addEventListener("hide.bs.collapse", stub);
 document.getElementById("single-input").addEventListener("show.bs.collapse", stub);
 document.getElementById("batch-input").addEventListener("show.bs.collapse", stub);
+document.getElementById("batch-input").addEventListener("hide.bs.collapse", stub);
+document.getElementById("single-copy").addEventListener("click", ()=>{copyResult('single')});
+document.getElementById("triple-copy").addEventListener("click", ()=>{copyResult('triple')});
 
 function stub() {
     console.log('stub function')
@@ -31,8 +34,25 @@ function validSingle() {
 }
 
 async function test() {
-    worker.postMessage(['test', 1, 2, 3]);
-    console.log('send message to worker');
+    new Toast('toast message');
+}
+
+function copyResult(copy) {
+    if (copy === 'single') {
+        let content = document.getElementById('result-single').innerText;
+        clipboard.writeText(content);
+        new Toast('Single Description Copied to Clipboard!');
+    } else {
+        let desc = []
+        let content = document.getElementById('result-triple-main').innerText;
+        desc.push(content);
+        content = document.getElementById('result-triple-ext1').innerText;
+        desc.push(content);
+        content = document.getElementById('result-triple-ext2').innerText;
+        desc.push(content);
+        clipboard.writeHTML(`<table><tbody><tr><td>${desc[0]}</td><td>${desc[1]}</td><td>${desc[2]}</td></tr></tbody></table>`);
+        new Toast('Triple Description Copied to Clipboard!');
+    }
 }
 
 async function progress() {
@@ -77,5 +97,26 @@ class ProgressBar {
             'percent': this.currentProgress,
             'message': this.progressText.innerText
         }
+    }
+}
+
+class Toast {
+    constructor(newMessage=null) {
+        this.toastContainer = document.getElementById('toastPlacement');
+        if (newMessage) {
+            this.newToast(newMessage);
+        }
+    }
+
+    newToast(message) {
+        let toast = document.createElement('div');
+        toast.setAttribute('class', "toast d-flex align-items-center border-0");
+        toast.innerHTML = `<div class="toast-body">${message}</div><button type="button" class="btn-close ms-auto me-2" data-bs-dismiss="toast"></button>`;
+        let bsToast = new bootstrap.Toast(toast);
+        this.toastContainer.appendChild(toast);
+        bsToast.show();
+        toast.addEventListener('hidden.bs.toast', (e) => {
+            e.target.remove();
+        })
     }
 }
