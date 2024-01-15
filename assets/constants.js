95194ee2ca0ce3e09254f9e2179953e50bbb21bb
desumai <desumai@github.com>
2024-01-15 16:02:30 -0500

    forgot to add a comment...

--- assets/constants.js
+++ assets/constants.js
@@ -2,7 +2,7 @@
 
 const CONSTANTS = Object.freeze({
     ENV: false ? 'prod.manage.prod' : 'test.manage.test', //set true to use production environment and set false for test environment.
-    OPEN_DEV_TOOLS: true,
+    OPEN_DEV_TOOLS: true, //set true to open dev tools on application launch
 });
 
 module.exports = CONSTANTS;
