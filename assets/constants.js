8896af0c026a43e39984bb68aea01a5d444a4fb7
desumai <desumai@github.com>
2024-01-16 11:10:29 -0500

    Change environment back to prod

--- assets/constants.js
+++ assets/constants.js
@@ -1,7 +1,7 @@
 //for global constants. used for debugging
 
 const CONSTANTS = Object.freeze({
-    ENV: false ? 'prod.manage.prod' : 'test.manage.test', //set true to use production environment and set false for test environment.
+    ENV: true ? 'prod.manage.prod' : 'test.manage.test', //set true to use production environment and set false for test environment.
     OPEN_DEV_TOOLS: true, //set true to open dev tools on application launch
 });
 
