150ef4dbc053228672a7b0b7297ed170b9da1b99
Jonathan Ma <jonathanmajh@gmail.com>
2024-03-26 16:38:28 -0400

    use only 1 variable for debug options

--- assets/constants.js
+++ assets/constants.js
@@ -1,22 +1,22 @@
 // for global constants. used for debugging
 
 const CONSTANTS = Object.freeze({
-    ENV: true ? 'prod.manage.prod' : 'development.manage.development', //set true to use production environment and set false for test environment.
-    OPEN_DEV_TOOLS: true, //set true to open dev tools on application launch
-    REPLACEMENTS: Object.freeze({ //for replacing commonly confused illegal characters with their legal counterparts 
-        //single quote
-        "`": "\'",
-        "´": "\'",
-        "‘": "\'",
-        "’": "\'",
-        //double quote
-        "“": '"',
-        "”": '"',
-        //hyphen
-        "—": "-",
-        "–": "-",
-        "−": "-",
-    })
+  OPEN_DEV_TOOLS: false, // set true to open dev tools on application launch
+  ENV: OPEN_DEV_TOOLS ? 'development.manage.development' : 'prod.manage.prod', // set true to use production environment and set false for test environment.
+  REPLACEMENTS: Object.freeze({ // for replacing commonly confused illegal characters with their legal counterparts
+    // single quote
+    '`': '\'',
+    '´': '\'',
+    '‘': '\'',
+    '’': '\'',
+    // double quote
+    '“': '"',
+    '”': '"',
+    // hyphen
+    '—': '-',
+    '–': '-',
+    '−': '-',
+  }),
 });
 
 module.exports = CONSTANTS;
