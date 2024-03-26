9350023dce9ef18b61390b5d54d172fd97c24bad
Jonathan Ma <jonathanmajh@gmail.com>
2024-03-26 16:28:32 -0400

    prod env

--- assets/constants.js
+++ assets/constants.js
@@ -1,7 +1,7 @@
 // for global constants. used for debugging
 
 const CONSTANTS = Object.freeze({
-    ENV: false ? 'prod.manage.prod' : 'development.manage.development', //set true to use production environment and set false for test environment.
+    ENV: true ? 'prod.manage.prod' : 'development.manage.development', //set true to use production environment and set false for test environment.
     OPEN_DEV_TOOLS: true, //set true to open dev tools on application launch
     REPLACEMENTS: Object.freeze({ //for replacing commonly confused illegal characters with their legal counterparts 
         //single quote
