3269c29167765bd36ab78bac7bb94de06a00edf5
Jonathan Ma <jonathanmajh@gmail.com>
2024-03-27 08:50:42 -0400

    oops

--- assets/constants.js
+++ assets/constants.js
@@ -2,7 +2,7 @@
 
 const CONSTANTS = Object.freeze({
   OPEN_DEV_TOOLS: false, // set true to open dev tools on application launch
-  ENV: OPEN_DEV_TOOLS ? 'development.manage.development' : 'prod.manage.prod', // set true to use production environment and set false for test environment.
+  ENV: this.OPEN_DEV_TOOLS ? 'development.manage.development' : 'prod.manage.prod', // set true to use production environment and set false for test environment.
   REPLACEMENTS: Object.freeze({ // for replacing commonly confused illegal characters with their legal counterparts
     // single quote
     '`': '\'',
