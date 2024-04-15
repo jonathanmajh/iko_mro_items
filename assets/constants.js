738d4979e9a8923a9645c8a7359054497b6aa6cc
desumai <desumai@github.com>
2024-04-15 15:55:08 -0400

    Commenting Constants
    added comments/docstrings for constants.js

--- assets/constants.js
+++ assets/constants.js
@@ -2,9 +2,23 @@
 
 const devMode = false; //set to true for testing. Remember to set it back to false when committing!
 
+/**
+ * Application constants
+ * @property {boolean} OPEN_DEV_TOOLS - if true, open dev tools on application launch
+ * @property {string} ENV - the current maximo environment (e.g. 'prod.manage.prod')
+ * @property {Map<string, string>} REPLACEMENTS - to replace commonly confused illegal characters with their legal counterparts in item search
+ * @property {Object} FIREBASECONFIG - firestore login info
+ * @property {string} FIRESTORE_EVENT_STARTAPP - string representing application startup event (firestore)
+ * @property {string} FIRESTORE_EVENT_SEARCH - string representing application item search event (firestore)
+ * @property {string} FIRESTORE_EVENT_ADDTOINVENTORY - string representing application adding item to user storeroom event (firestore)
+ * @property {string} FIRESTORE_EVENT_REQUESTITEM - string represention application requesting new item to corporate event (firestore)
+ */
 const CONSTANTS = Object.freeze({
-  OPEN_DEV_TOOLS: devMode, // set true to open dev tools on application launch
+  /** if true, open dev tools on application launch */
+  OPEN_DEV_TOOLS: devMode,
+  /** the current maximo environment (e.g. 'prod.manage.prod') */
   ENV: devMode ? 'test.manage.test' : 'prod.manage.prod', // set false to use production environment and set true for test environments.
+  /** to replace commonly confused illegal characters with their legal counterparts in item search */
   REPLACEMENTS: Object.freeze({
     // single quote
     '`': '\'',
@@ -19,6 +33,7 @@ const CONSTANTS = Object.freeze({
     '–': '-',
     '−': '-',
   }),
+  /** firestore login info */
   FIREBASECONFIG: {
     apiKey: 'AIzaSyArfJMEQvf1K2HEhlBixj-6CA_DqNC04bs',
     authDomain: 'iko-reliability.firebaseapp.com',
@@ -28,10 +43,14 @@ const CONSTANTS = Object.freeze({
     appId: '1:36956740284:web:561e9a73a0f3f4b08fceb9',
     measurementId: 'G-112KCDJT5G',
   },
+  /** string representing application startup event (firestore) */
   FIRESTORE_EVENT_STARTAPP: 'Start App',
+  /** string representing application item search event (firestore) */
   FIRESTORE_EVENT_SEARCH: 'Search Item',
+  /** string representing application add item to storeroom event (firestore) */
   FIRESTORE_EVENT_ADDTOINVENTORY: 'Add to Inventory',
-  FIRESTORE_EVENT_REQUESTITEM: 'Request Item',
+  /** string representing application request new item event (firestore) */
+  FIRESTORE_EVENT_REQUESTITEM: 'Request Item'
 });
 
 module.exports = CONSTANTS;
